94bb0a26d17057ca2db38216f76a6fddf899c80a
Izequias Silva <master@dedicado.digital>
2024-06-11 23:04:55 -0300

    feature: swagger incorpored

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,11 +7,15 @@ import {
 } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthLoginDto } from './auth.dto'
+import { ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOkResponse()
+  @ApiNotFoundResponse()
   @UseInterceptors(ClassSerializerInterceptor)
   @Post('login')
   async login(@Body() authLoginDto: AuthLoginDto) {
--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsString, MaxLength, MinLength } from 'class-validator'
 
 export class AuthLoginDto {
+  @ApiProperty()
   @IsString()
   @MinLength(10)
   @MaxLength(13)
   phone: string
 
+  @ApiProperty()
   @IsString()
   @MinLength(6)
   @MaxLength(6)

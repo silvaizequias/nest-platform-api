77a3e305e271fa30ab88184550f1164cacb30498
Izequias Silva <master@dedicado.digital>
2024-07-19 06:22:17 -0300

    update

--- src/organizations/organizations.module.ts
+++ src/organizations/organizations.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common'
 import { OrganizationsService } from './organizations.service'
 import { OrganizationsController } from './organizations.controller'
-import { LocationService } from 'src/location/location.service'
 
 @Module({
   imports: [],
   controllers: [OrganizationsController],
-  providers: [OrganizationsService, LocationService],
+  providers: [OrganizationsService],
   exports: [OrganizationsService],
 })
 export class OrganizationsModule {}
